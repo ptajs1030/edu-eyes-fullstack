ad6641afc7cfdd941f50781bff241eceb7595348
laodemuhff <ldmuhfarhanfy@gmail.com>
2025-05-04 19:14:44 +0700

    added seeder role,user & 1000 announcements

---
+++ database/factories/AnnouncementFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class AnnouncementFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        // fake announcement title and content
+        return [
+            'title' => fake()->text(),
+            'content' => fake()->text(),
+            'picture' => fake()->imageUrl(),
+        ];
+    }
+}
