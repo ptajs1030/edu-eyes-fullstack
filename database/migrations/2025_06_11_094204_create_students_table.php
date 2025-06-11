119751a1dc8fee645057d64ce0d4f8ad7d6f8bb8
Farhan Fauzan <ldmuhfarhanfy@gmail.com>
2025-06-11 20:19:20 +0700

    Add student, class, academic_year, class_history migration file

---
+++ database/migrations/2025_06_11_094204_create_students_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parent_id');
+            $table->unsignedBigInteger('class_id')->nullable();
+            $table->string('full_name');
+            $table->string('code')->nullable();
+            $table->year('entry_year');
+            $table->enum('gender', ['male', 'female']);
+            $table->enum('status', ['active', 'graduated', 'inactive']);
+            $table->date('date_of_birth');
+            $table->string('addresss');
+            $table->string('qr_code_url')->nullable();
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('users');
+            $table->foreign('class_id')->references('id')->on('classes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
---
+++ database/migrations/2025_06_11_130430_create_academic_years_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('academic_years', function (Blueprint $table) {
+            $table->id();
+            $table->year('start_year');
+            $table->string('title');
+            $table->enum('status', ['active', 'complete']);
+            $table->enum('attendance_mode', ['per-subject', 'per-shift']);
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('academic_years');
+    }
+};
