4e1d2e7d7909a62c20c985564442f8e0687a5ae4
muhamadsaid <zayd.developer@gmail.com>
2025-06-16 15:27:55 +0700

    sampel POST,GET dan LOGIN API

--- app/Services/AuthTeacherService.php
+++ app/Services/AuthTeacherService.php
@@ -2,7 +2,50 @@
 
 namespace App\Services;
 
+use App\DTOs\AuthData;
+use App\DTOs\UserData;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 class AuthTeacherService
 {
-    // Berisi tetang logic aplikasimu
+    public function addUser(UserData $data)
+    {
+        $user = User::create([
+            'full_name' => $data->getFullName(),
+            'username' => $data->getUsername(),
+            'email' => $data->getEmail(),
+            'phone' => $data->getPhone(),
+            'password' => Hash::make($data->getPassword()),
+            'role_id' => 1,
+        ]);
+        return $user;
+    }
+
+    public function showWithLogic()
+    {
+        // masukkan logic disini lalu di return sesuai Modelnya
+        $users = User::where('role_id', 2)->get();
+
+        if ($users->isEmpty()) {
+            abort(204, "Data Kosong"); // atau gunakan kode lain seperti 204, 422, dsb sesuai kebutuhan
+        }
+
+        return $users;
+    }
+
+    public function login(AuthData $body)
+    {
+        $user = User::where('username', $body->getUsername())->first();
+
+        if (!$user || !Hash::check($body->getPassword(), $user->password)) {
+            abort(401, 'Username or password is incorrect');
+        }
+        Auth::login($user);
+        return [
+            'token' => $user->createToken('authToken')->plainTextToken,
+            'user' => $user
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\AuthTeacherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('sample')->group(function (){
+    Route::get('/user/{id?}', [AuthTeacherController::class, 'show']);
+    Route::get('/user-logic', [AuthTeacherController::class, 'showWithLogic']);
+    Route::post('/user', [AuthTeacherController::class, 'addUser']);
+    Route::post('/login', [AuthTeacherController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/me', [AuthTeacherController::class, 'user']);
+
+    });
+});
