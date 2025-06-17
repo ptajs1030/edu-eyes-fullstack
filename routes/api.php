ac78d6e7d1b8de9ada1e6db1b9d14866a3961171
NaufalDanadyaksa <naufal060906@gmail.com>
2025-06-17 17:39:41 +0700

    create middleware and auth for teacher (undone)

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,31 @@
 <?php
 
 use App\Http\Controllers\Api\AuthTeacherController;
+use App\Http\Controllers\Api\SampleAuthTeacherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::prefix('sample')->group(function (){
-    Route::get('/user/{id?}', [AuthTeacherController::class, 'show']);
-    Route::get('/user-logic', [AuthTeacherController::class, 'showWithLogic']);
-    Route::post('/user', [AuthTeacherController::class, 'addUser']);
+// Route::prefix('sample')->group(function (){
+//     Route::get('/user/{id?}', [SampleAuthTeacherController::class, 'show']);
+//     Route::get('/user-logic', [SampleAuthTeacherController::class, 'showWithLogic']);
+//     Route::post('/user', [SampleAuthTeacherController::class, 'addUser']);
+//     Route::post('/login', [SampleAuthTeacherController::class, 'login']);
+
+//     Route::middleware('auth:sanctum')->group(function () {
+//         Route::get('/me', [SampleAuthTeacherController::class, 'user']);
+
+//     });
+// });
+
+
+Route::prefix('auth/teacher')->controller(AuthTeacherController::class)->group(function (){
     Route::post('/login', [AuthTeacherController::class, 'login']);
 
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/me', [AuthTeacherController::class, 'user']);
-
+    Route::middleware(['auth:sanctum', 'teacher'])->group(function () {
+        Route::post('/logout', [AuthTeacherController::class, 'logout']);
     });
 });
