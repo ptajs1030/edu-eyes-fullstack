0bcb1d5166f013a7412e4799664cf38466e2701c
Farhan Fauzan <ldmuhfarhanfy@gmail.com>
2025-06-17 17:57:39 +0700

    FIX bug realtion between shifting_attendances and student. FIX seed file on student

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Classroom;
+use App\Models\Role;
 use App\Models\Student;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -20,11 +21,13 @@ class StudentSeeder extends Seeder
 
         $classV = Classroom::where('name', 'Class V A')->first();
         $classVI = Classroom::where('name', 'Class VI A')->first();
+        $parentRoleID = Role::where('name', 'parent')->first()->id;
+        $parentData = User::where('role_id', $parentRoleID)->first();
 
         // Seed 10 students for Class V A
         for ($i = 0; $i < 10; $i++) {
             Student::create([
-                'parent_id' => User::where('role_id', 3)->first()->id,
+                'parent_id' => $parentData->id,
                 'class_id' => $classV->id,
                 'full_name' => $faker->name,
                 'entry_year' => 2024,
