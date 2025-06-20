2751bc76f74e12abea038ccf5398e285d80fa938
NaufalDanadyaksa <naufal060906@gmail.com>
2025-06-20 14:06:12 +0700

    create get attendance history all or by date (done)

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -12,8 +12,8 @@ class AttendanceController extends BaseApiController
     public function __construct(protected AttendanceService $service){}
 
 
-    public function attendanceHistory(){
-        $data = $this->service->attendanceHistory();
+    public function attendanceHistory(?string $date=null){
+        $data = $this->service->attendanceHistory($date);
     
         return $this->success([
             'number_of_attendances' => $data['number_of_attendances'],
