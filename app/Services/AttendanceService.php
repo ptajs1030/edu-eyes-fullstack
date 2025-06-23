55dee410346558aaa021609af756f377f8cb94fe
NaufalDanadyaksa <naufal060906@gmail.com>
2025-06-23 22:27:51 +0700

    bugs on shiftingAttendance

--- app/Services/AttendanceService.php
+++ app/Services/AttendanceService.php
@@ -14,15 +14,23 @@ use Carbon\Carbon;
 
 class AttendanceService
 {
-    public function attendanceHistory($date){
+    public function attendanceHistory($date, $class_id){
        
-        $date=Carbon::parse($date)->format('Y-m-d');
+        $query = ShiftingAttendance::query();
 
-        if($date){
-            $attendances = ShiftingAttendance::where('submit_date', $date)->get();
-        }else {
-            $attendances = ShiftingAttendance::whereDate('submit_date', Carbon::today())->get();
+        if ($date && $date !== 'null') {
+            $parsedDate = Carbon::parse($date)->format('Y-m-d');
+            $query->where('submit_date', $parsedDate);
         }
+    
+        if ($class_id) {
+            $query->whereHas('classShiftingSchedule', function ($q) use ($class_id) {
+                $q->where('class_id', $class_id);
+            });
+        }
+        $query->where('clock_out_hour', '!=', null);
+              
+        $attendances = $query->paginate(10);
 
         if ($attendances->isEmpty()) {
             abort(204, "Data Kosong"); 
@@ -31,28 +39,30 @@ class AttendanceService
         
 
         return [
-            'number_of_attendances' => $attendances->count(),
-            'attendances' => $attendances,
+            'number_of_attendances' => $attendances->total(),
+            'current_page' => $attendances->currentPage(),
+            'last_page' => $attendances->lastPage(),
+            'per_page' => $attendances->perPage(),
+            'attendances' => $attendances->items(),
         ];
     }
 
-    public function shiftingAttendance(ShiftingAttendanceData $data){
+    public function shiftingAttendance($student_id){
         $day= ClassShiftingSchedule::where('day', Carbon::now()->dayOfWeek)->first();
         
         $pic = ClassShiftingSchedulePic::where('class_shifting_schedule_id', $day->id)->where('teacher_id', auth()->user()->id)->value('class_shifting_schedule_id');
 
-        $attendace=ShiftingAttendance::where('student_id', $data->getStudent())->first();
+        $attendace=ShiftingAttendance::where('student_id', $student_id)->first();
         $late_tolerance = (int)Setting::where('key', 'late_tolerance')->value('value');
         $class_shifting_schedule=ClassShiftingSchedule::where('id', $attendace->class_shifting_schedule_id)->first();
-        $start_hour_raw = Shifting::where('id', $class_shifting_schedule->id)->value('start_hour');
+        $start_hour_raw = Shifting::where('id', $class_shifting_schedule->shifting_id)->value('start_hour');
         $start_hour = Carbon::parse(Carbon::today()->format('Y-m-d') . ' ' . $start_hour_raw, 'Asia/Jakarta');
         $deadline = $start_hour->copy()->addMinutes($late_tolerance);
         $submit_hour = Carbon::now()->setTimezone('Asia/Jakarta');
 
         $minutes_of_late = $deadline->diffInMinutes($submit_hour);
 
-        
-
+dd($pic, $attendace);
       
         if(!$attendace){
             abort(404, 'Student not found');
@@ -60,23 +70,30 @@ class AttendanceService
             abort(403, 'You are not allowed to access this student');
         }
 
-        if ($submit_hour > $deadline) {
+        if ($submit_hour <= $deadline && !$attendace->clock_in_hour) {
+            $attendace->update([
+                'status' => 'present',
+                'clock_in_hour' => $submit_hour->format('H:i'),
+            ]);
+        }else if ($submit_hour > $deadline && !$attendace->clock_in_hour) {
+
             $attendace->update([
                 'status' => 'late',
                 'minutes_of_late' => (int) $minutes_of_late,
-                'submit_hour' => $submit_hour->format('H:i'),
+                'clock_in_hour' => $submit_hour->format('H:i'),
             ]);
         }else {
             $attendace->update([
-                'status' => 'present',
-                'submit_hour' => $submit_hour->format('H:i'),
+                'clock_out_hour' => $submit_hour->format('H:i'),
+               
             ]);
         }
+
+       
 
         return [
             'message' => 'Attendance updated successfully',
             'attendance' => $attendace,
-
         ];
 
         
