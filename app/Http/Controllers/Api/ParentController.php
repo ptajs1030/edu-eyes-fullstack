fef1b1019c1e399eb03c76b3742d251449c77d63
NaufalDanadyaksa <naufal060906@gmail.com>
2025-06-26 15:58:57 +0700

    create getAnnouncements, getStudents, getAttendancHistory for parent (undone)

--- app/Http/Controllers/Api/ParentController.php
+++ app/Http/Controllers/Api/ParentController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ChangePasswordRequest;
+use App\Http\Resources\AnnouncementResource;
+use App\Http\Resources\StudentResource;
 use App\Http\Resources\UserResource;
+use App\Models\Announcement;
+use App\Models\Student;
 use App\Services\ParentService;
 use Illuminate\Http\Request;
 
@@ -18,5 +22,30 @@ class ParentController extends BaseApiController
 
     public function changePassword(ChangePasswordRequest $data){
         return $this->success($this->service->changePassword($data->getDto()));
+    }
+
+    public function getStudents(){
+        return $this->resource(StudentResource::collection(Student::where('parent_id', auth()->user()->id)->get()));
+    }
+
+    public function getAnnouncements(?int $id = null)
+    {
+        if ($id) {
+            return $this->resource(AnnouncementResource::make(Announcement::findOrFail($id)));
+        }
+        return $this->resource(AnnouncementResource::collection(Announcement::paginate(10)->getCollection()));
+    }
+
+    public function todayAttendance(Request $request){
+        $student = $request->attributes->get('current_student');
+       
+        return $this->success($this->service->todayAttendance($student));
+    }
+
+    public function attendanceHistory(Request $request, ?string $date=null){
+
+        $student = $request->attributes->get('current_student');
+        
+        return $this->success($this->service->attendanceHistory($date, $student));
     }
 }
