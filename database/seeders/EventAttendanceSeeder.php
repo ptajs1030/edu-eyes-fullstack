37bd7e853beafea4565cb0649c7b6f0026c815cc
Farhan Fauzan <ldmuhfarhanfy@gmail.com>
2025-07-11 03:08:57 +0700

    Fixed error eventAttendanceSeeder

--- database/seeders/EventAttendanceSeeder.php
+++ database/seeders/EventAttendanceSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\AcademicYear;
 use App\Models\Event;
 use App\Models\EventAttendance;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -30,10 +31,12 @@ class EventAttendanceSeeder extends Seeder
                     'event_id' => $event->id,
                     'academic_year_id' => $academicYear->id,
                     'submit_date' => now(),
-                    'clock_in_hour' => $status === 'present' ? $event->start_hour->addMinutes(rand(0, 30)) : null,
-                    'clock_out_hour' => $status === 'present' ? $event->end_hour->subMinutes(rand(0, 30)) : null,
+                    'clock_in_hour' => $status === 'present_in_tolerance'
+                        ? Carbon::parse($event->start_hour)->addMinutes(rand(0, 5))
+                        : Carbon::parse($event->start_hour),
+                    'clock_out_hour' => Carbon::parse($event->end_hour)->addMinutes(rand(0, 10)),
                     'status' => $status,
-                    'minutes_of_late' => $status === 'present' ? rand(0, 30) : null,
+                    'minutes_of_late' => $status === 'present' ? null : rand(0, 15),
                     'note' => $status !== 'present' ? 'Alasan: ' . fake()->sentence() : null,
                 ]);
             }
