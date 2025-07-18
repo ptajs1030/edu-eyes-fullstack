62676997a5b9c3df5b9ff4176d5d35331e40e7a2
laodemuhff <ldmuhfarhanfy@gmail.com>
2025-07-18 15:26:36 +0700

    remove register

---
+++ public/build/assets/text-link-D9nYbwtp.js
@@ -0,0 +1,1 @@
+import{j as t,Y as n}from"./app-DlK1hqPj.js";import{a}from"./app-logo-icon-bz7doTEx.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
