93b20531fe29cc27ce948d6c75ee6071ce7bdfbe
laodemuhff <ldmuhfarhanfy@gmail.com>
2025-07-19 17:11:01 +0700

    remove forgot password

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -60,11 +60,11 @@ export default function Login({ status, canResetPassword }: LoginProps) {
                     <div className="grid gap-2">
                         <div className="flex items-center">
                             <Label htmlFor="password">Password</Label>
-                            {canResetPassword && (
+                            {/* {canResetPassword && (
                                 <TextLink href={route('password.request')} className="ml-auto text-sm" tabIndex={5}>
                                     Forgot password?
                                 </TextLink>
-                            )}
+                            )} */}
                         </div>
                         <Input
                             id="password"
