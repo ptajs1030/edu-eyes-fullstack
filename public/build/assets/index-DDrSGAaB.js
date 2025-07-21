91a583d8921a6e2a7867e15643ff7a9007f585c8
laodemuhff <ldmuhfarhanfy@gmail.com>
2025-07-21 07:47:36 +0700

    bugfix create student & history student

--- public/build/assets/index-DDrSGAaB.js
+++
@@ -1,1 +0,0 @@
-import{g as r,b as t}from"./app-wAfF6O0U.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ public/build/assets/success-BuvN4eac.js
@@ -0,0 +1,6 @@
+import{j as e,$ as t}from"./app-V1Umm8Rk.js";import{A as c}from"./auth-layout-bhw_5fmr.js";import{c as r}from"./app-logo-icon-B1P7fFv2.js";/* empty css            *//**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]],a=r("CircleCheck",o);function m({status:s}){return e.jsxs(c,{title:"Password Reset Successful",description:"Your password has been successfully updated. You can now log in with your new password.",children:[e.jsx(t,{title:"Password Reset Success"}),e.jsxs("div",{className:"flex flex-col items-center gap-6 text-center",children:[e.jsx("div",{className:"rounded-full bg-green-100 p-4 text-green-600",children:e.jsx(a,{className:"h-8 w-8",strokeWidth:2})}),e.jsx("div",{className:"space-y-2",children:e.jsx("p",{className:"text-muted-foreground text-sm",children:"please login with your new password."})}),s&&e.jsx("div",{className:"mt-4 text-center text-sm font-medium text-green-600",children:s})]})]})}export{m as default};
