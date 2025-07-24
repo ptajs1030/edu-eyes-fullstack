db5195af035ef6cdc14a9b3e36e8685764bc1ecc
Farhan Fauzan <ldmuhfarhanfy@gmail.com>
2025-07-25 01:35:00 +0700

    modified style user

--- resources/js/pages/students/form.tsx
+++ resources/js/pages/students/form.tsx
@@ -338,7 +338,6 @@ export default function StudentFormModal({ isOpen, onClose, student, classrooms,
                 />
             </div>
             <div className="flex justify-end">
-                {/* <button type="button" onClick={onClose} className="mr-2 rounded bg-gray-500 px-4 py-2 text-white hover:cursor-pointer"> */}
                 <button
                     type="button"
                     onClick={onClose}
