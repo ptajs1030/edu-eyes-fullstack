8384507cfc0dea9a0e21d306772f23b3f171d712
NaufalDanadyaksa <naufal060906@gmail.com>
2025-07-25 15:38:39 +0700

    create ExamService (undone) and ExamSeeder

---
+++ app/Http/Controllers/Api/ExamController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\ExamService;
+use Illuminate\Http\Request;
+
+class ExamController extends BaseApiController
+{
+    public function __construct(protected ExamService $service){}
+
+    public function getSubject(Request $request){
+        $student= $request->attributes->get('current_student');
+        $date= $request->query('date');
+        $search= $request->query('search');
+        return $this->success($this->service->getExam($student, $date, $search));
+    }
+
+    public function getExam(Request $request, int $subject){
+        $student= $request->attributes->get('current_student');
+        $date=$request->query('date');
+        return $this->success($this->service->getExam($student, $date, $subject));
+    }
+}
