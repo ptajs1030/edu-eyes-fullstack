992147e929329fa75613ae2f1de2073a682c6792
laodemuhff <ldmuhfarhanfy@gmail.com>
2025-07-30 20:05:13 +0700

    build

--- public/build/assets/pagination-Bfb9rFVr.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/pagination-S8gYe8TZ.js
-import{j as e,a as s}from"./app-BAPB98s5.js";function u({links:r}){return e.jsx("div",{className:"mt-4 flex justify-center gap-2",children:r.map((t,a)=>e.jsx("button",{onClick:()=>t.url&&s.visit(t.url),disabled:!t.url,className:`rounded px-3 py-1 text-sm hover:cursor-pointer ${t.active?"bg-blue-500 text-white":"bg-gray-100 text-black"}`,dangerouslySetInnerHTML:{__html:t.label}},a))})}export{u as P};
-========
-import{j as e,a as s}from"./app-BoAFDmgT.js";function u({links:r}){return e.jsx("div",{className:"mt-4 flex justify-center gap-2",children:r.map((t,a)=>e.jsx("button",{onClick:()=>t.url&&s.visit(t.url),disabled:!t.url,className:`rounded px-3 py-1 text-sm hover:cursor-pointer ${t.active?"bg-blue-500 text-white":"bg-gray-100 text-black"}`,dangerouslySetInnerHTML:{__html:t.label}},a))})}export{u as P};
->>>>>>>> 1b2ffb39f2123cc942cb8b251ae222b3efecde11:public/build/assets/pagination-Bfb9rFVr.js
