9bd2e3f68d5721c967b3ccf4f61a6d71cbae2c14
NaufalDanadyaksa <naufal060906@gmail.com>
2025-08-09 18:40:40 +0700

    create getUnpaidPayment for parent and bug fixing

--- app/Http/Controllers/Api/ParentController.php
+++ app/Http/Controllers/Api/ParentController.php
@@ -138,4 +138,9 @@ class ParentController extends BaseApiController
             'payments' => $data['payments'],
         ]);
     }
+
+    public function getUnpaidPayment(Request $request){
+        $student = $request->attributes->get('current_student');
+        return $this->success($this->service->getUnpaidPayment($student));
+    }
 }
