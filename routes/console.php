e08bc97cf13e452af1e5e6e8b4d14e4e5fa8ade8
ptajs1030 <ptajs.dev@gmail.com>
2025-08-10 17:03:30 +0700

    Update console.php add without overlapping

--- routes/console.php
+++ routes/console.php
@@ -7,18 +7,22 @@ use Illuminate\Support\Facades\Schedule;
 app()->make(App\Console\Commands\MakeService::class);
 app()->make(App\Console\Commands\MakeDto::class);
 
+// Schedule::command('attendance:generate-shifting')
+//     //->dailyAt('00:00')
+//     // ->everyFiveMinutes()
+//     ->everyMinute()
+//     ->timezone('Asia/Jakarta')
+//     ->before(function () {
+//         Log::info('[Scheduler] Mulai jalanin generate-shifting');
+//     })
+//     ->after(function () {
+//         Log::info('[Scheduler] Selesai jalanin generate-shifting');
+//     })
+//     ->appendOutputTo(storage_path('logs/attendance-error.log'));
 Schedule::command('attendance:generate-shifting')
-    //->dailyAt('00:00')
-    // ->everyFiveMinutes()
     ->everyMinute()
-    ->timezone('Asia/Jakarta')
-    ->before(function () {
-        Log::info('[Scheduler] Mulai jalanin generate-shifting');
-    })
-    ->after(function () {
-        Log::info('[Scheduler] Selesai jalanin generate-shifting');
-    })
-    ->appendOutputTo(storage_path('logs/attendance-error.log'));
+    ->withoutOverlapping()
+    ->timezone('Asia/Jakarta');
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
