129ed9c5a81e98904b6b298ad4c1b79ea1b22507
Farhan Fauzan <ldmuhfarhanfy@gmail.com>
2025-08-26 15:58:25 +0700

    remove unused import

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,7 +8,6 @@ use App\Enums\StudentStatus;
 use App\Models\Classroom;
 use App\Models\Student;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
