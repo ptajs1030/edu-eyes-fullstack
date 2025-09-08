e9068bb7d4f45953ca5983e856df28eca288e518
NaufalDanadyaksa <naufal060906@gmail.com>
2025-09-08 13:15:44 +0700

    build

--- public/build/assets/pagination-DQHh2ioN.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/pagination-DQHh2ioN.js
-import{j as e,a as s}from"./app-Cn2t8gJw.js";function u({links:r}){return e.jsx("div",{className:"mt-4 flex justify-center gap-2",children:r.map((t,a)=>e.jsx("button",{onClick:()=>t.url&&s.visit(t.url),disabled:!t.url,className:`rounded px-3 py-1 text-sm hover:cursor-pointer ${t.active?"bg-blue-500 text-white":"bg-gray-100 text-black"}`,dangerouslySetInnerHTML:{__html:t.label}},a))})}export{u as P};
-========
-import{j as e,a as s}from"./app-BbluYI2S.js";function u({links:r}){return e.jsx("div",{className:"mt-4 flex justify-center gap-2",children:r.map((t,a)=>e.jsx("button",{onClick:()=>t.url&&s.visit(t.url),disabled:!t.url,className:`rounded px-3 py-1 text-sm hover:cursor-pointer ${t.active?"bg-blue-500 text-white":"bg-gray-100 text-black"}`,dangerouslySetInnerHTML:{__html:t.label}},a))})}export{u as P};
->>>>>>>> main:public/build/assets/pagination-DQE4fwPp.js
--- public/build/assets/text-link-r7mkQIe5.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/text-link-068gMVUa.js
-import{j as t,L as n}from"./app-Cn2t8gJw.js";import{c as a}from"./button-B_qfFY_x.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
-========
-import{j as t,L as n}from"./app-BbluYI2S.js";import{c as a}from"./button-CcOSGNeD.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
->>>>>>>> main:public/build/assets/text-link-r7mkQIe5.js
