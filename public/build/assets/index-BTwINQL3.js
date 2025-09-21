99387262b7b1ef08852b5621cd6f663bbce9c099
Farhan Fauzan AVO <farhanfauzanavo@MacBook-Pro-avo.local>
2025-09-22 03:35:14 +0700

    handle several bugs

--- public/build/assets/index-BTwINQL3.js
+++
@@ -1,1 +0,0 @@
-import{z as r,E as t}from"./app-PwX3OJdg.js";var a=t();const e=r(a);export{e as R,a as r};
--- public/build/assets/text-link-BX68_r5W.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/text-link-BX68_r5W.js
-import{j as t,L as n}from"./app-BvWbc2t8.js";import{c as a}from"./button-DpO31YqY.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
-========
-import{j as t,L as n}from"./app-PwX3OJdg.js";import{c as a}from"./button-BGb3s8-p.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
->>>>>>>> e8de2cb59887f04153199d77ab0278aca389ab59:public/build/assets/text-link-OQNIBZvj.js
