2ef792679f2773d0db30f39f744671a45311d246
Farhan Fauzan AVO <farhanfauzanavo@MacBook-Pro-avo.local>
2025-09-22 01:30:38 +0700

    translate pesan sukses & error, dan fix export all data siswa

---
+++ public/build/assets/index-BTwINQL3.js
@@ -0,0 +1,1 @@
+import{z as r,E as t}from"./app-PwX3OJdg.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ public/build/assets/success-DkcO9_Kh.js
@@ -0,0 +1,6 @@
+import{j as e,H as r}from"./app-PwX3OJdg.js";import{A as a}from"./auth-layout-C-QDKP42.js";import{c as t}from"./createLucideIcon-DAC-9vYY.js";/* empty css            */import"./app-logo-icon-CFbv-uI0.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]],c=t("CircleCheck",i);function x({status:s}){return e.jsxs(a,{title:"Password Berhasil Diperbarui",description:"Password anda berhasil diperbarui silahkan login dengan password terbaru anda.",children:[e.jsx(r,{title:"Password Berhasil Diperbarui"}),e.jsxs("div",{className:"flex flex-col items-center gap-6 text-center",children:[e.jsx("div",{className:"rounded-full bg-green-100 p-4 text-green-600",children:e.jsx(c,{className:"h-8 w-8",strokeWidth:2})}),e.jsx("div",{className:"space-y-2"}),s&&e.jsx("div",{className:"mt-4 text-center text-sm font-medium text-green-600",children:s})]})]})}export{x as default};
