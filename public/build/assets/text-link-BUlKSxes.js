22f84fccd87e8f80506432aa68bb311e1785d152
Farhan Fauzan AVO <farhanfauzanavo@MacBook-Pro-avo.local>
2025-09-22 05:07:08 +0700

    fix export all user data

--- public/build/assets/text-link-BUlKSxes.js
+++
@@ -1,1 +0,0 @@
-import{j as t,L as n}from"./app-R0rMdquo.js";import{c as a}from"./button-C2viNmP0.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
