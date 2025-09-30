d24dc61b3f6245ae0290d9d9572c6e39d99ce720
Farhan Fauzan AVO <farhanfauzanavo@MacBook-Pro-avo.local>
2025-09-30 19:09:48 +0700

    vommit

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -6,7 +6,7 @@ export default function AppLogo({ small = false }: { small?: boolean }) {
     return (
         <>
             <div className={`text-sidebar-primary-foreground flex aspect-square items-center justify-center rounded-md ${small ? 'size-8' : 'size-12'}`}>
-                <AppLogoIcon logoUrl={schoolLogo} className={small ? 'size-4 fill-current text-white dark:text-white' : 'size-5 fill-current text-white dark:text-white'} />
+                <AppLogoIcon logoUrl={schoolLogo} className={small ? 'size-2 fill-current text-white dark:text-white' : 'size-5 fill-current text-white dark:text-white'} />
             </div>
             {!small && (
                 <div className="ml-1 grid flex-1 text-left text-sm">
