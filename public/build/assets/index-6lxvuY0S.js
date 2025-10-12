5335ba885f3d1b97a97e2d6ede954a0cd932f8af
Farhan Fauzan AVO <farhanfauzanavo@MacBook-Pro-avo.local>
2025-10-12 23:01:05 +0700

    build

--- public/build/assets/index-6lxvuY0S.js
+++
@@ -1,1 +0,0 @@
-import{B as r,E as t}from"./app-Cy_yH0cU.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ public/build/assets/index-BfdFR44v.js
@@ -0,0 +1,1 @@
+import{B as r,E as t}from"./app-BBgxHzmc.js";var a=t();const e=r(a);export{e as R,a as r};
